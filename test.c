d6ba634574f1d166fd5efae70a8c615f2d146632
Josh <jfsbq6@mst.edu>
2018-04-30 21:39:09 -0500

    Add spooky color changing ghost triangle
    Much spooks

--- test.c
+++ test.c
@@ -84,7 +84,17 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        float current_time = glfwGetTime();
+        float offset = (sin(current_time) / 2.0f) + 0.5;
+        int ho_location = glGetUniformLocation(tri_shader.id, "horizontal_offset");
+
+        if(ho_location == -1) {
+            printf("Could not find horizontal offset uniform\n");
+            return -1;
+        }
+
         glUseProgram(tri_shader.id);
+        glUniform1f(ho_location, offset);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
--- vertexshader.glsl
+++ vertexshader.glsl
@@ -5,8 +5,10 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 out vec3 ourColor;
 
+uniform float horizontal_offset;
+
 void main() {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-   ourColor = aColor;
+   gl_Position = vec4(aPos.x + horizontal_offset, aPos.y, aPos.z, 1.0);
+   ourColor = vec3(aColor.xyz + horizontal_offset);
 }
 
