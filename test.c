7d555e8039736a945b8e13332c6be38e2f137fea
Josh <jfsbq6@mst.edu>
2018-05-01 20:11:42 -0500

    Move window init code to function
    It was taking up a lot of space in my main function, so i moved it.

--- test.c
+++ test.c
@@ -11,30 +11,12 @@
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 void glfw_error_callback(int code, const char *err_str);
+GLFWwindow *initializeWindow();
 
 int main() {
     printf("Hello, World!\n");
 
-    glfwSetErrorCallback(glfw_error_callback);
-
-    if(!glfwInit()) {
-        printf("error initializing GLFW\n");
-        return -1;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow *window = glfwCreateWindow(800, 600, "TITLE", NULL, NULL);
-
-    if(window == NULL) {
-        printf("Error creating window\n");
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    GLFWwindow *window = initializeWindow();
 
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         printf("Failed to initiate GLAD\n");
@@ -123,4 +105,29 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 
 void glfw_error_callback(int code, const char *err_str) {
     printf("GLFW error: \n\tcode: 0x%x\n\t%s\n", code, err_str);
+}
+
+GLFWwindow *initializeWindow() {
+    glfwSetErrorCallback(glfw_error_callback);
+
+    if(!glfwInit()) {
+        printf("error initializing GLFW\n");
+        exit(1);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "TITLE", NULL, NULL);
+
+    if(window == NULL) {
+        printf("Error creating window\n");
+        glfwTerminate();
+        exit(1);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    return window;
 }
