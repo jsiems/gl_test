01aa7630104bad1a0de6d5f63622ac981e837cec
Josh <jfsbq6@mst.edu>
2018-06-02 22:29:17 -0500

    Add basic lighting

--- shaders/light_vs.glsl
+++ shaders/light_vs.glsl
@@ -2,10 +2,6 @@
 #version 330 core
 
 layout (location = 0) in vec3 pos;
-layout (location = 1) in vec2 tex_coord_in;
-
-//OUTPUT MUST HAVE THE ****EXACT**** SAME NAME AS INPUT TO FRAGMENT SHADER
-out vec2 tex_coord;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -13,6 +9,5 @@ uniform mat4 projection;
 
 void main() {
    gl_Position = projection * view * model * vec4(pos, 1.0);
-   tex_coord = vec2(tex_coord_in.x, tex_coord_in.y);
 }
 
--- test.c
+++ test.c
@@ -25,10 +25,6 @@ void scroll_callback(GLFWwindow* window, double x_offset, double y_offset);
 void glfw_error_callback(int code, const char *err_str);
 GLFWwindow *initializeWindow();
 
-float mixture = 0.0f;
-int up_pressed = 0;
-int down_pressed = 0;
-
 float delta_time = 0.0f;
 float last_frame = 0.0f;
 
@@ -49,9 +45,13 @@ int main() {
     //enable depth test
     glEnable(GL_DEPTH_TEST);
     //initialize the shaders
-    struct shader shader_program;
-    if(!initializeShader(&shader_program, "vertexshader.glsl", "fragmentshader.glsl")) {
-        printf("Error initializing shader_program\n");
+    struct shader object_shader, light_shader;
+    if(!initializeShader(&object_shader, "shaders/object_vs.glsl", "shaders/object_fs.glsl")) {
+        printf("Error initializing object shaders\n");
+        return -1;
+    }
+    if(!initializeShader(&light_shader, "shaders/light_vs.glsl", "shaders/light_fs.glsl")) {
+        printf("Error initializing light shader\n");
         return -1;
     }
 
@@ -59,101 +59,51 @@ int main() {
     //initialize the camera
     initializeCamera(&cam, (vec3){0.0f, 0.0f, 3.0f}, (vec3){0.0f, 1.0f, 0.0f}, -90.0f, 0.0f, 2.5f, 0.1f, 45.0f);
 
-    //load textures
-    int image_width, image_height, nr_channels;
-    unsigned char *image_data;
-    unsigned int texture0, texture1;       //texture id given by opengl
-
-    //box
-    glGenTextures(1, &texture0);
-    glBindTexture(GL_TEXTURE_2D, texture0);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    //load image data
-    stbi_set_flip_vertically_on_load(1);
-    image_data = stbi_load("container.jpg", &image_width, &image_height, &nr_channels, 0);
-    if(image_data != NULL) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_width, image_height, 0, GL_RGB, GL_UNSIGNED_BYTE, image_data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else {
-        printf("Failed to load texture container\n");
-        return -1;
-    }
-    stbi_image_free(image_data);
-
-    //happy face
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    //load image data
-    image_data = stbi_load("awesomeface.png", &image_width, &image_height, &nr_channels, 0);
-    if(image_data != NULL) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else {
-        printf("Failed to load texture awesome face\n");
-        return -1;
-    }
-    stbi_image_free(image_data);
-
-
     //-------------vertex data and buffers
 
     float vertices[] = {
-        //positions           //texture coordinates
-         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        //positions
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
 
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
 
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
 
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
 
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    };
-
-    unsigned int indices[] = {
-        0, 1, 3,
-        1, 2, 3
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
     };
 
     vec3 cubes[] = {
@@ -169,30 +119,31 @@ int main() {
         {-1.3f,  1.0f, -1.5f},
     };
 
-    //vertex array object, stores vertex buffer object and vertex attribute data
-    unsigned int VAO, VBO, EBO;
+    vec3 light = {1.2f, 1.0f, 2.0f};
+
+    //cube Vertex array object n such
+    unsigned int VAO, VBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     //bind the vertex array object
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    //texture attributes
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
 
-    glUseProgram(shader_program.id);
-    glUniform1i(glGetUniformLocation(shader_program.id, "texture0"), 0);
-    glUniform1i(glGetUniformLocation(shader_program.id, "texture1"), 1);
+    //lamp vertex array object
+    unsigned int lightVAO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //we don't need to buffer data for VBO because it has alreayd been done
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     //Main loop
     while(!glfwWindowShouldClose(window)) {
@@ -205,37 +156,42 @@ int main() {
         processInput(window, &cam);
 
         //rendering commands here
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //bind textures
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture0);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-
-        //use shader program
-        glUseProgram(shader_program.id);
-        glUniform1f(glGetUniformLocation(shader_program.id, "mixture"), mixture);
-        glBindVertexArray(VAO);
-
-        //constructs a lookat view matrix
-        mat4 view;
+        //construct matrices for camera
+        mat4 model, view, projection;
         getViewMatrix(&cam, view);
-        glUniformMatrix4fv(glGetUniformLocation(shader_program.id, "view"), 1, GL_FALSE, (GLfloat *)view);
-
-        //constructs the projection matrix
-        mat4 projection;
         glm_perspective(degToRad(cam.zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f, projection);
-        glUniformMatrix4fv(glGetUniformLocation(shader_program.id, "projection"), 1, GL_FALSE, (GLfloat *)projection);
+
+
+        // LIGHT UNIFORMS && DRAWING
+        //Configure uniforms and draw vertices for light
+        glUseProgram(light_shader.id);
+        glBindVertexArray(lightVAO);
+        glUniformMatrix4fv(glGetUniformLocation(light_shader.id, "view"), 1, GL_FALSE, (GLfloat *)view);
+        glUniformMatrix4fv(glGetUniformLocation(light_shader.id, "projection"), 1, GL_FALSE, (GLfloat *)projection);
+        glm_translate_make(model, light);
+        glm_scale(model, (vec3){0.2f, 0.2f, 0.2f});
+        glUniformMatrix4fv(glGetUniformLocation(light_shader.id, "model"), 1, GL_FALSE, (GLfloat *)model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+        //OBJECTS UNIFORMS && DRAWING
+        //Configure uniforms and draw vertices for cubes
+        glUseProgram(object_shader.id);
+        glBindVertexArray(VAO);
+        glUniformMatrix4fv(glGetUniformLocation(object_shader.id, "view"), 1, GL_FALSE, (GLfloat *)view);
+        glUniformMatrix4fv(glGetUniformLocation(object_shader.id, "projection"), 1, GL_FALSE, (GLfloat *)projection);
+        //sets the objects color and the color of the light hitting the object
+        glUniform3fv(glGetUniformLocation(object_shader.id, "object_color"), 1, (vec3){1.0f, 0.5f, 0.31f});
+        glUniform3fv(glGetUniformLocation(object_shader.id, "light_color"), 1, (vec3){1.0f, 1.0f, 1.0f});
 
         for(int i = 0; i < 10; i ++) {
             mat4 model;
             glm_translate_make(model, cubes[i]);
             glm_rotate(model, i * degToRad(20.0f) + (float)glfwGetTime() * degToRad(50.0f), (vec3){0.5f, 1.0f, 0.0f});
-
-            glUniformMatrix4fv(glGetUniformLocation(shader_program.id, "model"), 1, GL_FALSE, (GLfloat *)model);
-
+            glUniformMatrix4fv(glGetUniformLocation(object_shader.id, "model"), 1, GL_FALSE, (GLfloat *)model);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
@@ -247,8 +203,8 @@ int main() {
     }
 
     glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &lightVAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
@@ -260,35 +216,14 @@ int main() {
 //  in the loop
 void processInput(GLFWwindow *window, struct camera *cam) {
     int escape = glfwGetKey(window, GLFW_KEY_ESCAPE);
-    int up = glfwGetKey(window, GLFW_KEY_UP);
-    int down = glfwGetKey(window, GLFW_KEY_DOWN);
     int w = glfwGetKey(window, GLFW_KEY_W);
     int a = glfwGetKey(window, GLFW_KEY_A);
     int s = glfwGetKey(window, GLFW_KEY_S);
     int d = glfwGetKey(window, GLFW_KEY_D);
 
+    //quit when escape is pressed
     if(escape == GLFW_PRESS)
         glfwSetWindowShouldClose(window, 1U);
-    if(up == GLFW_PRESS && !up_pressed) {
-        mixture += 0.1f;
-        up_pressed = 1;
-    }
-    else if(up == GLFW_RELEASE && up_pressed) {
-        up_pressed = 0;
-    }
-    if(down == GLFW_PRESS && !down_pressed) {
-        mixture -= 0.1f;
-        down_pressed = 1;
-    }
-    else if(down == GLFW_RELEASE && down_pressed) {
-        down_pressed = 0;
-    }
-    if(mixture < 0.0f) {
-        mixture = 0.0f;
-    }
-    if(mixture > 1.0f) {
-        mixture = 1.0f;
-    }
 
     if(w == GLFW_PRESS) {
         translateCamera(cam, cam_forward, delta_time);
